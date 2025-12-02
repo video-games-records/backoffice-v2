a1eeb842a1001d8cf84c552f3370104fa56f31ee
magicbart <magicbart@gmail.com>
2025-12-02 21:52:53 +0100

    Fix dwh api

--- src/BoundedContext/VideoGamesRecords/Dwh/Presentation/Api/Resource/Player.php
+++ src/BoundedContext/VideoGamesRecords/Dwh/Presentation/Api/Resource/Player.php
@@ -7,46 +7,28 @@ namespace App\BoundedContext\VideoGamesRecords\Dwh\Presentation\Api\Resource;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\OpenApi\Model;
-use VideoGamesRecords\DwhBundle\Controller\Player\GetPositions;
-use VideoGamesRecords\DwhBundle\Controller\Player\GetMedalsByTime;
+use App\BoundedContext\VideoGamesRecords\Dwh\Presentation\Api\Controller\Player\GetPositions;
+use App\BoundedContext\VideoGamesRecords\Dwh\Presentation\Api\Controller\Player\GetMedalsByTime;
 
 #[ApiResource(
     operations: [
         new Get(
             uriTemplate: '/players/{id}/get-positions',
             controller: GetPositions::class,
-            read: false,
             openapi: new Model\Operation(
                 summary: 'Return player dwh stats positions',
                 description: 'Return player dwh stats positions',
             ),
-            /*openapiContext: [
-            'parameters' => [
-            [
-            'name' => 'id',
-            'in' => 'path',
-            'type' => 'int'
-            ]
-            ]
-            ]*/
+            read: false,
         ),
         new Get(
             uriTemplate: '/players/{id}/get-medals-by-time',
             controller: GetMedalsByTime::class,
-            read: false,
             openapi: new Model\Operation(
                 summary: 'Return player dwh stats medals',
                 description: 'Return player dwh stats medals',
             ),
-            /*openapiContext: [
-            'parameters' => [
-            [
-            'name' => 'id',
-            'in' => 'path',
-            'type' => 'int'
-            ]
-            ]
-            ]*/
+            read: false,
         )
     ],
 )]
